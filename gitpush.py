b3f20d4f43bebab7214229c4e38d5edd0a7691e6
sw4pnilsingh <singhswapnil070107@gmail.com>
2025-09-03 00:01:44 +0530

    Add files via upload

---
+++ gitpush.py
@@ -0,0 +1,11 @@
+# WAP to perform various operations on two no. taken from the user
+a=int(input("ENTER THE FIRST NO."))
+b=int(input("ENTER THE SECOND NO."))
+sum=a+b
+sub=a-b
+mul=a*b
+sqr=a**2,b**2
+print("THE ADDITION OF THESE No.IS",sum)
+print("THE SUBTRACTION OF THESE No. IS",sub)
+print("THE PRODUCT OF THESE No. IS",mul)
+print("THE SQUARE OF THESE No. IS",sqr)
